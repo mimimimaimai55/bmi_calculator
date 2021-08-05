74b67e2c58eba1a76fa2c491002c69a22179b028
mizuhomai <choco.hana.choco@gmail.com>
2021-08-05 16:23:16 +0900

    セクション12-131 最後まで完了 身長のスライダー完成

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -81,17 +81,25 @@ class _InputPageState extends State<InputPage> {
                         )
                       ],
                     ),
-                    Slider(
-                      value: height.toDouble(),
-                      min: 120.0,
-                      max: 220.0,
-                      activeColor: Color(0xffeb1555),
-                      inactiveColor: Color(0xff8d8e98),
-                      onChanged: (double newValue){
-                        setState(() {
-                          height =  newValue.round();
-                        });
-                      },
+                    SliderTheme(
+                      data: SliderTheme.of(context).copyWith(
+                        inactiveTrackColor:  Color(0xff8d8e98),
+                        activeTrackColor: Colors.white,
+                        thumbColor: Color(0xffeb1555),
+                        overlayColor: Color(0x29eb1555),
+                        thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                        overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0),
+                      ),
+                      child: Slider(
+                        value: height.toDouble(),
+                        min: 120.0,
+                        max: 220.0,
+                        onChanged: (double newValue){
+                          setState(() {
+                            height =  newValue.round();
+                          });
+                        },
+                      ),
                     ),
                   ],
                 )
