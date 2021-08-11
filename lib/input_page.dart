13ba3a0d1cfb0c5bba837d5078cc92f6340976a3
mizuhomai <choco.hana.choco@gmail.com>
2021-08-12 01:20:27 +0900

    セクション12の134が終了 ２ページ目の外側のみ作成。これから中身を作っていく

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -3,6 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'icon_content.dart';
 import 'reusable_card.dart';
 import 'constants.dart';
+import 'results_page.dart';
 
 enum Gender {
   male,
@@ -200,11 +201,21 @@ class _InputPageState extends State<InputPage> {
                     ),
                   ],
                 )),
-            Container(
-              color: kBottomContainerColour,
-              margin: EdgeInsets.only(top: 10.0),
-              width: double.infinity,
-              height: kBottomContainerHeight,
+            GestureDetector(
+              onTap:(){
+                Navigator.push(context,MaterialPageRoute(builder: (context) => ResultsPage()));
+              },
+              child: Container(
+                child: Center(
+                    child: Text('CALCULATE',style: kLargeButtonTextStyle,
+                    ),
+                ),
+                color: kBottomContainerColour,
+                margin: EdgeInsets.only(top: 10.0),
+                padding: EdgeInsets.only(bottom: 20.0),
+                width: double.infinity,
+                height: kBottomContainerHeight,
+              ),
             ),
           ],
         ),
